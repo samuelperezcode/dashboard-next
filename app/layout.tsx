5824629ff79a472c37b729488bc4bc387776c597
SamyCode <sambea2103@gmail.com>
2023-11-04 16:52:10 -0400

    Add fonts and images and fix styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { montserrat } from './ui/font';
+import './ui/global.css'
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
