e531bea345f1c8900c3a8baf77ec1eec485bd5b7
SamyCode <sambea2103@gmail.com>
2023-11-16 17:19:04 -0500

    Create createInvoice action and schema

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,20 @@
 'use server'
 
+import { sql } from "@vercel/postgres"
+import { CreateInvoice } from "./schemas"
+ 
+
 export async function createInvoice(formData:FormData) {
-  const rowFromData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  }
-  console.log(rowFromData)
+  })
+  const amountInCents = amount * 100
+  const date = new Date().toString().split('T')[0]
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `
 }
