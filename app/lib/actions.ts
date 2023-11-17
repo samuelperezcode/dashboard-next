91ede2f9fbfa0a844322debfdd6b8920e7dcb1bd
SamyCode <sambea2103@gmail.com>
2023-11-16 21:49:02 -0500

    Add form validation
    Add useFormState to create-form.tsx component.
    Edit the createInvoice action to handle validation errors from Zod.
    Display the errors in your component, and add aria labels to improve accessibility.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,13 +5,30 @@ import { CreateInvoice, UpdateInvoice } from "./schemas"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
  
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-export async function createInvoice(formData:FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State ,formData:FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  const { amount, customerId, status} = validatedFields.data
+
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
   console.log(date)
