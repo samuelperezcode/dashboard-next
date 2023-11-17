e8aa9d8dbc9adc8ee336441f3908b39e066fd58e
SamyCode <sambea2103@gmail.com>
2023-11-16 22:45:50 -0500

    Create authenticate server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { sql } from "@vercel/postgres"
 import { CreateInvoice, UpdateInvoice } from "./schemas"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
+import { signIn } from "@/auth"
  
 export type State = {
   errors?: {
@@ -80,4 +81,18 @@ export async function deleteInvoice(id: string){
   }
   
   revalidatePath('/dashboard/invoices')
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  }
 }
